39cb947919213a6cafe42e60a9aad69613aa3b41
O-vier21 <131209730+O-vier21@users.noreply.github.com>
2023-05-07 16:09:15 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
+global.owner = ['657255084']
+global.premium = ['657255084']
+global.ownernomer = '657255084'
+global.ownername = 'OLI'
+global.botname = 'BIXBY-MD OLI FORK'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
 global.region = 'India, South India, Kerala'
